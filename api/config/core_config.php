fea86898d60bd1c2d4b2eebc698532c8309ca36c
Adam Foster <adam.foster93@hotmail.co.uk>
2018-08-28 19:51:21 +0100

    Refactor config class to return just itself

--- api/config/core_config.php
+++ api/config/core_config.php
@@ -2,25 +2,22 @@
 include_once 'database.php';
 class CoreConfig{
 
-
     public function __construct(){
-        $this->getConfig();
+        return $this->getConfig();
     }
 
     public function getConfig(){
 
         $db = new Database();
-        $this->config = new stdClass();
-
         $query = "SELECT * FROM config";
         $stmt = $db->conn->prepare($query);
         $stmt->execute();
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-            $this->config->{$row['config_key']} = $row['config_value'];
+            $this->{$row['config_key']} = $row['config_value'];
         }
 
-        return $this->config;
+        return $this;
     }
 }
 ?>
