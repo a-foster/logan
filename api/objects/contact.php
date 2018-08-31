2e4fc0a245d8e5f30c6383d544c1d7e4b59793df
Adam Foster <adam.foster93@hotmail.co.uk>
2018-08-31 11:55:14 +0100

    Added Contact functions for CRUD operations

--- api/objects/contact.php
+++ api/objects/contact.php
@@ -26,4 +26,36 @@ class Contact{
         // add record but return boolean success status
         return $this->logan->db->runStatement("INSERT INTO contacts ($columns) VALUES ($values)");
     }
+
+    // probably only need to update contact by email at this point
+    function updateContact($email, $params) {
+
+        $changes = '';
+
+        foreach ($params as $col => $val) {
+            if (!$changes) { $changes = "$col='$val'"; }
+            else { $changes .= ",$col='$val'"; }
+
+        }
+
+        return $this->logan->db->runStatement("UPDATE contacts SET $changes WHERE email='$email'");
+    }
+
+    function deleteContact($email) {
+        return $this->logan->db->runStatement("DELETE FROM contacts WHERE email='$email'");
+    }
+
+    function getContacts($params) {
+
+        $wheres = '';
+
+        foreach ($params as $col => $val) {
+            if (!$wheres) { $wheres = "$col='$val'"; }
+            else { $wheres .= "AND $col='$val'"; }
+
+        }
+
+        // add record but return boolean success status
+        return $this->logan->db->runQuery("SELECT * FROM contacts WHERE $wheres");
+    }
 }
--- api/tests/contact_test.php
+++ api/tests/contact_test.php
@@ -3,5 +3,7 @@ include_once '../objects/contact.php';
 
 $contact = new Contact();
 
-echo $contact->addContact(array('email' => 'logan@test.com', 'first_name' => 'Ben', 'surname' => 'Dover', 'phone_number' => '0123456789', 'city' => 'Johannesburg', 'is_friend' => 'true'));
+// echo $contact->addContact(array('email' => 'logan@test.com', 'first_name' => 'Ben', 'surname' => 'Dover', 'phone_number' => '0123456789', 'city' => 'Johannesburg', 'is_friend' => 'true'));
+
+echo var_dump($contact->getContacts(array('is_friend' => 'true')));
  ?>
