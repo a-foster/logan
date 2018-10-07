4b59188c3617a9bd6c1ee890d91654bc6b03c11c
Adam Foster <adamfoster@Adams-MacBook-Pro.local>
2018-10-07 20:41:34 +0100

    Rename hardware triggers to event triggers for abstraction

--- api/objects/hardware_trigger.php
+++ api/objects/hardware_trigger.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'logan.php';
 
-class HardwareTrigger{
+class EventTrigger{
 
     public function __construct(){
 
@@ -12,14 +12,14 @@ class HardwareTrigger{
     function addTrigger($trigger_type, $trigger_time) {
 
         // add record but return success status
-        return $this->logan->db->runStatement("INSERT INTO hardware_triggers (trigger_type, trigger_time)
+        return $this->logan->db->runStatement("INSERT INTO event_triggers (trigger_type, trigger_time)
                                               VALUES ('$trigger_type', '$trigger_time')");
     }
 
     // For now all we need to be able to update is the 'triggered' column
     function resetTriggers($trigger_type) {
 
-        return $this->logan->db->runStatement("UPDATE hardware_triggers SET triggered=NULL WHERE trigger_type=$trigger_type");
+        return $this->logan->db->runStatement("UPDATE event_triggers SET triggered=NULL WHERE trigger_type=$trigger_type");
     }
 
     // TODO - as above
@@ -30,13 +30,13 @@ class HardwareTrigger{
     // Return all trigger times for a given event
     function getTriggers($trigger_type) {
 
-        return $this->logan->db->runQuery("SELECT * FROM hardware_triggers where trigger_type = $trigger_type");
+        return $this->logan->db->runQuery("SELECT * FROM event_triggers where trigger_type = $trigger_type");
     }
 
     // Determine if event should be triggered based on current time and triggered flag
     function shouldTrigger($trigger_type) {
 
         $curr_time = date("H:i:s");
-        return $this->logan->db->runQuery("SELECT * FROM hardware_triggers where trigger_type = $trigger_type");
+        return $this->logan->db->runQuery("SELECT * FROM event_triggers where trigger_type = $trigger_type");
     }
 }
