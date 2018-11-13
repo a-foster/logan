9b505abb2009c4dd4cb961e1ff5ccce593cc80b4
a-foster <adam.foster93@hotmail.co.uk>
2018-11-13 17:29:51 +0000

    Added logging get_logs endpoint to retrieve logs by type and number

--- api/config/logging.php
+++ api/config/logging.php
@@ -8,7 +8,24 @@ class Logging{
     }
 
     public function writeLog($type, $message){
-      return $this->db->runStatement("INSERT INTO logging (log_type, log_message) VALUES ('$type', '$message')");
+        return $this->db->runStatement("INSERT INTO logging (log_type, log_message) VALUES ('$type', '$message')");
+    }
+
+    // Return logs of a certain type (or all), limit to certain number (or all)
+    function getLogs($type, $limit) {
+
+        $sql = "SELECT * FROM logging";
+
+        if ($type != '*') {
+            $sql .= " WHERE type='$type'";
+        }
+
+        if ($limit != '*') {
+            $limit = intval($limit);
+            $sql .= " LIMIT $limit";
+        }
+
+        return $this->db->runQuery($sql);
     }
 }
 ?>
