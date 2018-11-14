dc5c7feceba36c9fd7f7bdf4b706055d56be639f
a-foster <adam.foster93@hotmail.co.uk>
2018-11-14 10:38:39 +0000

    Remove obsolete test

--- api/tests/contact_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once '../objects/contact.php';
-
-$contact = new Contact();
-
-// echo $contact->addContact(array('email' => 'logan@test.com', 'first_name' => 'Ben', 'surname' => 'Dover', 'phone_number' => '0123456789', 'city' => 'Johannesburg', 'is_friend' => 'true'));
-
-echo $contact->updateContact('logan@test.com', array('is_colleague' => '1', 'is_test' => '1'));
- ?>
